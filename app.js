10a23a67d756efac8a167c56ea9ff004587bd1dd
stead-yujin <70509403+yujch716@users.noreply.github.com>
2023-07-23 21:50:01 +0900

    express 기초 - express 설치 - 라우트

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3100
+
+app.get('/', (req, res) => {
+    res.send('Home page!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('Login page!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
