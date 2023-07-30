a29f544a04bcb95290d3efbc67a2074840cb8d0b
stead-yujin <70509403+yujch716@users.noreply.github.com>
2023-07-30 19:50:44 +0900

    express - 템플릿 엔진

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3100
+app.locals.pretty = true; //코드 예쁘게 보이기
 app.use(express.static('public')); //정적인 화면 출력
+
+//템플릿 엔진 사용
+app.set('view engine', 'pug');
+app.set('views','./views');
+app.get('/template', function (req,res){
+    res.render('temp', {title:'pug', comment:'hihi'}); //두번째 인자로 객체 전달
+})
 
 app.get('/', (req, res) => {
     res.send('Home page!')
